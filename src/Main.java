9cf7061896824ca9d688d4d4b2b3d09762973061
FelipeRiba <feliperibadev@gmail.com>
2023-03-07 12:09:42 -0300

    Criação das classes Curso e Mentoria. Criação e instanciação dos objetos curso1, curso2 e mentoria1.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso java");
+        curso1.setDescricao("Curso para desenvolvedor Java Júnior - FelipeRibaDev");
+        curso1.setCargaHoraria(40);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso javascript");
+        curso2.setDescricao("Curso para desenvolvedor JavaScript - FelipeRibaDev");
+        curso2.setCargaHoraria(80);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria Java");
+        mentoria1.setDescricao("Mentoria para desenvolvedor Java Júnior - FelipeRibaDev");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
     }
 }
