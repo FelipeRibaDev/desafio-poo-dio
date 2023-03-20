36448699e19415cbf1484097bc1b31d5001c4a7e
FelipeRiba <feliperibadev@gmail.com>
2023-03-20 11:28:17 -0300

    Final - na classe DEV adicionamos as ações dos métodos inscreverBootcamp, progredir e calcularTotalXp (conforme plano de negócios).
    No Main, criamos um Bootcamp e dois Devs para testar o projeto.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -20,8 +22,44 @@ public class Main {
         mentoria1.setDescricao("Mentoria para desenvolvedor Java Júnior - FelipeRibaDev");
         mentoria1.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria1);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria1);
+
+        Bootcamp bootCampJava = new Bootcamp();
+        bootCampJava.setNome("Bootcamp JAVA 2023");
+        bootCampJava.setDescricao("Bootcamp para formação de desenvolvedor JAVA");
+        bootCampJava.getConteudos().add(curso1);
+        bootCampJava.getConteudos().add(curso2);
+        bootCampJava.getConteudos().add(mentoria1);
+
+        Dev devFelipe = new Dev();
+        devFelipe.setNome("Felipe Riba");
+        devFelipe.inscreverBootcamp(bootCampJava);
+        System.out.println("Conteúdos Inscritos Felipe: " + devFelipe.getConteudosInscritos());
+        System.out.println("XP ---- " + devFelipe.calcularTotalXp());
+        System.out.println("-");
+        devFelipe.progredir();
+        System.out.println("Conteúdos Concluídos Felipe: " + devFelipe.getConteudosConcluidos());
+        System.out.println("Conteúdos Inscritos Felipe: " + devFelipe.getConteudosInscritos());
+        System.out.println("XP ---- " + devFelipe.calcularTotalXp());
+
+        System.out.println("------------------");
+
+        Dev devBenicio = new Dev();
+        devBenicio.setNome("Benicio Riba");
+        devBenicio.inscreverBootcamp(bootCampJava);
+        System.out.println("Conteúdos Inscritos Benício: " + devBenicio.getConteudosInscritos());
+        System.out.println("XP ---- " + devBenicio.calcularTotalXp());
+        System.out.println("-");
+        devBenicio.progredir();
+        devBenicio.progredir();
+        devBenicio.progredir();
+        System.out.println("Conteúdos Inscritos Benício: " + devBenicio.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Benício: " + devBenicio.getConteudosConcluidos());
+        System.out.println("XP ---- " + devBenicio.calcularTotalXp());
+
+
+
     }
 }
